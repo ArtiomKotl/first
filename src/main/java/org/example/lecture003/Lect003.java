4486e4a26f900911ed71b1db8249bb275d4ff27a
artketler <artketler.ak@gmail.com>
2024-10-04 22:18:57 +0300

    Java Study lection 3

--- src/main/java/org/example/lecture003/Lect003.java
+++ src/main/java/org/example/lecture003/Lect003.java
@@ -7,5 +7,12 @@ public class Lect003 {
         if (a < 100) {
             System.out.println("a less than 100");
         }
+
+        int b = 110;
+        int c = 20;
+
+        if ((b >= 100 && c < 50) || (b < 1000 && c > 20 )) {
+            System.out.println ( " !!!!!!! ");
+        }
     }
 }
